f5ea370d4939079df331eaead9e60378ca7b1f98
abdulahi roble <abdulahi_2610@hotmail.com>
2023-05-30 23:37:28 +0200

    Cleanup and add schema

--- bank_project/urls.py
+++ bank_project/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('bank_app.urls'))
+    path('', include('bank_app.urls')),
+    path('api/', include_docs_urls(title='APIs')),
+    path('api/accounts/', include('bank_app.urls')),  # Assuming you have account-related APIs in 'bank_app.urls'
+    path('api/loans/', include('bank_app.urls')),  
 ]
